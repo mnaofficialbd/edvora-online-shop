de3380c4dbbdf7d8013c9f14a853df51f23e5fcc
mnaofficial <mna.official92@gmail.com>
2022-08-19 16:55:51 +0600

    banner setup

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/004/299/813/small/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-vector.jpg" alt="Banner" />
+        </div>
+    );
+};
+
+export default Banner;
