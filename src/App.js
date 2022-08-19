c585844010f75689ae3a088ed5f7ad267f7802cd
mnaofficial <mna.official92@gmail.com>
2022-08-19 09:52:41 +0600

    Route setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { Route, Routes } from 'react-router-dom';
 import Navber from '../src/Pages/Home/Navber/Navber';
 import Footer from './Pages/Home/Footer';
+import Home from './Pages/Home/Home';
+import Products from './Pages/Products/Products';
 
 function App() {
   return (
     <>
       <Navber />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
 
-      
+          {/* <Route path='purchase/:productId' element={<RequireAuth><PurchaseDetail /></RequireAuth>} />
+          <Route path='orders/:productId' element={<RequireAuth><Purchase /></RequireAuth>} />
+
+          <Route path='dashboard' element={<RequireAuth><Dashboard /></RequireAuth>} >
+            <Route index element={<MyProfile/>} />
+            <Route path='orders' element={<Orders/>} />
+            <Route path='addReview' element={<AddAReview/>} />
+            <Route path='manageAllOrders' element={<ManageAllOrders/>} />
+            <Route path='addProduct' element={<AddAProduct/>} />
+            <Route path='makeAdminPanel' element={<MakeAdminPanel/>} />
+            <Route path='manageProducts' element={<ManageProducts/>} />
+          </Route>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} /> */}
+
+          {/* <Route path='*' element={<NotFound />} /> */}
+        </Routes>
+
       <Footer />
     </>
   );
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Products from '../Products/Products';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <Products />
         </div>
     );
 };
