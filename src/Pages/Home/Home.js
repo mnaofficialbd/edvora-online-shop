d6b7362d72ac479387af5c1e303fda95565724ea
mnaofficial <mna.official92@gmail.com>
2022-08-19 19:55:36 +0600

    Done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <>
             <Banner />
             <Product />
-            <BusinessSummery />
+            {/* <BusinessSummery /> */}
             <Reviews />
         </>
     );
